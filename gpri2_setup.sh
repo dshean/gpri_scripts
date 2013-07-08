53c0ef147ac71a394f3db03406fbdd5d6de91ee7
David Shean <dshean@gmail.com>
2013-07-07 22:10:36 -0700

    wip

--- gpri2_setup.sh
+++ gpri2_setup.sh
@@ -5,12 +5,19 @@
 #Mount volume
 #set mntdir = /Volumes/dshean_bb/MtRainier_GPRI
 mntdir=~/mnt/dshean/sw
-topdir=$mntdir/gpri2_usr
+topdir=$mntdir/gpri2_root
 
 if [[ -z "$LD_LIBRARY_PATH" ]]; then
   #export LD_LIBRARY_PATH
-  export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:/usr/local/lib
+  #export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/lib/x86_64-linux-gnu:/usr/local/lib
+  echo "LD not defined"
 fi
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$topdir/lib
-export PATH=${PATH}:$topdir/bin
+#export PATH=${PATH}:$topdir/bin:$topdir/usr/bin
+#export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$topdir/lib:$topdir/usr/lib
+#export PATH=$topdir/bin:$topdir/usr/bin:${PATH}
+#export LD_LIBRARY_PATH=$topdir/lib:$topdir/usr/lib:${LD_LIBRARY_PATH}
+#export PATH=$topdir/usr/bin:${PATH}
+#export LD_LIBRARY_PATH=$topdir/usr/lib:${LD_LIBRARY_PATH}
+export PATH=${PATH}:$topdir/usr/bin
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$topdir/usr/lib
--- gpri_all.sh
+++ gpri_all.sh
@@ -4,6 +4,8 @@
 #dshean@gmail.com
 #12/19/12
 
+set -x
+
 #Basic workflow to process GPRI data
 
 #Want to set up checks for existing data to avoid unnecessary reprocessing and overwriting
@@ -11,14 +13,18 @@
 
 #Set reference coordinates for unwrapping
 #These were picked for Lower Nisqually, using data from the June roi survey, rlks 15
-r_ref=153 
-a_ref=459
+#r_ref=153 
+#a_ref=459
+
+r_ref=100 
+a_ref=100
 
 #Needed to set up local environment for GAMMA software install
 DIR="$( cd "$( dirname "$0" )" && pwd )" 
 source $DIR/gpri2_setup.sh
 
-slcdir=slc_dir
+#slcdir=slc/20130628
+slcdir=slc
 
 #Create table of slc
 mk_tab $slcdir slc slc.par slc_tab
@@ -32,7 +38,7 @@ mlidir=mli_dir_${rlks}
 #Create mli
 mk_mli_all slc_tab $mlidir $rlks $azlks 0 0.7 0.35
 
-step=4
+step=6
 stride=1
 start=1
 
@@ -40,10 +46,11 @@ start=1
 mk_itab itab $(cat slc_tab | wc -l) $step $stride $start 
 
 #Generate interferograms
-mk_int_2d slc_tab itab $(ls $mlidir/*l.mli | head -1) $mlidir diff_${rlks} $rlks $azlks 3 0 0 1 -s 0.7 -e 0.35
+#Note: gpri usr/bin/head is HEAD
+mk_int_2d slc_tab itab $(ls $mlidir/*l.mli | /usr/bin/head -1) $mlidir diff_${rlks} $rlks $azlks 3 0 0 1 -s 0.7 -e 0.35
 
 #Filter w/ adaptive bandpass filter
-mk_adf_2d slc_tab itab $(ls $mlidir/*l.mli | head -1) diff_${rlks} 5 0.4 32 4 -s 0.7 -e 0.35
+mk_adf_2d slc_tab itab $(ls $mlidir/*l.mli | /usr/bin/head -1) diff_${rlks} 5 0.4 32 4 -s 0.7 -e 0.35
 
 #Set thresholds for correlation score and relative intensity
 #set c_thr = 0.2
@@ -52,7 +59,7 @@ c_thr=0.7
 i_thr=0
 
 #Unwrap interferograms
-mk_unw_2d slc_tab itab $(ls $mlidir/*l.mli | head -1) diff_${rlks} $c_thr $i_thr 1 1 1 1 $r_ref $a_ref 1 -d diff_tab_${rlks} -s 0.7 -e 0.35 -p 1.0
+mk_unw_2d slc_tab itab $(ls $mlidir/*l.mli | /usr/bin/head -1) diff_${rlks} $c_thr $i_thr 1 1 1 1 $r_ref $a_ref 1 -d diff_tab_${rlks} -s 0.7 -e 0.35 -p 1.0
 
 #Check for availability of convert first
 #Convert ras to png and rotate
